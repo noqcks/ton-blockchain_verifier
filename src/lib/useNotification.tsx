2d1063d8412813dfbb829866a0709e51d5046cd9
Benji Visser <benji@093b.org>
2024-04-04 03:31:45 +0000

    Upgrade dependencies

--- src/lib/useNotification.tsx
+++ src/lib/useNotification.tsx
@@ -23,7 +23,7 @@ function useNotification() {
       onClose?: () => void,
       autoHideDuration?: number,
     ) => {
-      let key: SnackbarKey | undefined; key = enqueueSnackbar(<StyledMessage>{message}</StyledMessage>, {
+      let key: any | undefined; key = enqueueSnackbar(<StyledMessage>{message}</StyledMessage>, {
         variant,
         autoHideDuration: autoHideDuration || 5000,
         onClose,
